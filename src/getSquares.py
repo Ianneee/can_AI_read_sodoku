377d658baa2002238d5bd88978cf02a4e9f7550e
Ian <iantirso.cini@gmail.com>
2023-08-15 15:18:02 +0200

    refactor and clean

--- src/getSquares.py
+++ src/getSquares.py
@@ -5,12 +5,10 @@ from typing import Union
 
 #Funzione che ottiene gli 81 quadratini come singole immagini in una lista
 def getSquares(video: Union[None, cv2.VideoCapture] = None, img_path: Union[None, str] = None):
-        # Catturo dalla fotocamera
         if not video and not img_path:
             raise AttributeError("You need to provide at least one argument.")
 
         if video:
-            # video = cv2.VideoCapture(0)
             _, img = video.read()
         elif img_path:
             img = cv2.imread(img_path)
@@ -28,10 +26,6 @@ def getSquares(video: Union[None, cv2.VideoCapture] = None, img_path: Union[None
         warped = getBirdEye(img,src_points) #Sudoku dall'alto
         warped = cv2.resize(warped, (540, 540))
 
-        cv2.imshow("Sudoku!", warped)
-        cv2.waitKey(1)
-        cv2.displayStatusBar("Sudoku!", 'Press k for close this window', 0)
-
         #ottengo i singoli quadratini del sudoku dividendo in 9 righe uguali l'immagine
         righe = np.vsplit(warped,9)
         quadratini = [] #lista di immagini da restituire
@@ -41,13 +35,7 @@ def getSquares(video: Union[None, cv2.VideoCapture] = None, img_path: Union[None
                 for quadrato in colonne:
                         quadratini.append(quadrato)
 
-        #mostro l'immagine ed esco premendo f ; si può usare per debug
-        #cv2.imshow("grid",quadratini[0])
-        #if cv2.waitKey(0) and 0xff == ord('f'):
-        #        cv2.destroyAllWindows()
-
-
-        return quadratini
+        return quadratini, warped
 
 
 #Funzione che ottiene i contorni dell'immagine passata
@@ -65,52 +53,6 @@ def getContours(img):
         contours, hierarchy = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
         return contours
 
-
-
-
-
-#Funzione che prende le coordinate dei quattro angoli identificando il contorno del sudoku nell'immagine
-def getSrcPoints_o(img,contours):
-        #scelgo soltanto il quadrato esterno della griglia del sudoku
-    for x in contours:
-        if cv2.contourArea(x) > 90000:
-            # print(cv2.contourArea(x))
-            epsilon = 0.01 * cv2.arcLength(x,True) #Calcolo l'epsilon per la semplificazine del poligono
-            approx = cv2.approxPolyDP(x,epsilon,True) #Approssimo ad un poligono
-            if len(approx) ==4: #proseguo solo se è un rettangolo
-                #cv2.drawContours(img,[approx],0,(0,255,0),2) #disegno sull'immagine (Utile se si vuole printare per debug)
-                n = approx.ravel() #appiattisco l'array (Equivalente di shape -1)
-                middle = img.shape[1]//2 #metà della larghezza dell'immagine
-                src_points = np.zeros((4,2)) #array che conterrà le coordinate degli angoli ordinate
-                listaSX = np.zeros((2,2))
-                listaDX = np.zeros((2,2))
-                #algoritmo per ordinare le coordinate degli angoli:
-                #si trovano i due angoli più a sinistra e si ordinano dal più alto al più basso
-                #lo stesso vale per i due angoli a destra
-                #alla fine si inseriscono prima quelli di sinistra e poi quelli di destra nell'array finale.
-                for i,y in enumerate(n):
-                    if i%2 == 0:
-                        if y < middle:
-                            if listaSX[0][0] == 0:
-                                listaSX[0] = (y,n[i+1])
-                            else:
-                                if listaSX[0][1] < n[i+1]:
-                                    listaSX[1] = (y,n[i+1])
-                                else:
-                                    listaSX[1] = listaSX[0]
-                                    listaSX[0] = (y,n[i+1])
-                        else:
-                            if listaDX[0][0] == 0:
-                                listaDX[0] = (y,n[i+1])
-                            else:
-                                if listaDX[0][1] < n[i+1]:
-                                    listaDX[1] = (y,n[i+1])
-                                else:
-                                    listaDX[1] = listaDX[0]
-                                    listaDX[0] = (y,n[i+1])
-                src_points = np.array([listaSX[0],listaSX[1],listaDX[0],listaDX[1]], dtype = np.float32) #array che contiene le coordinate degli angoli ordinate (UpperSX,LowerSX,UpperDX,LowerDX)
-                break
-    return src_points
 
 def getSrcPoints(contours, area=True):
     if area:
@@ -176,5 +118,3 @@ def getBirdEye(img,src_points):
         warped = cv2.warpPerspective(img,BE,(900,900))
         return warped
 
-if __name__ == "__main__":
-    getSquares(img_path="webcam.jpeg")
